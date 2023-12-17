ece766b4c50f9fb67e155dbc9a09153fff999ede
walid bettahar <walidbettahar18@gmail.com>
2023-12-17 19:15:25 +0100

    first commit

---
+++ api_songs/internal/swagger_expose.go
@@ -0,0 +1,11 @@
+package internal
+
+// @title        middleware
+// @version      1.0.0
+// @description  API to manage songs.
+
+// @contact.name   walid bettahar & mahdi falek .
+// @contact.email  walidbettahar18@gmail.com
+
+// @schemes   http
+// @BasePath  /
